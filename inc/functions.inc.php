6fde0413307e0eeb33fbd57b485d29b0057969dd
Charles Julian Knight <charles@s0journ-viii.rabidaudio.dnynds.org>
2013-03-02 17:03:39 -0500

    Added data update functions
    specifically has_user_updated, user_has_updated, and update_info

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -15,11 +15,10 @@ function get_db_name($eventid){
 
 function get_user_info($userid, $eventid){
 //Take a userid and eventid, and return an array of user data
-	#echo "get_user_info called";
 
 	$eventdb=get_db_name($eventid);
-
-	$get_users = "select * from `$eventdb` where user_id=$userid limit 0,1";
+	$userid=mysql_real_escape_string($userid);
+	$get_users = "select * from `$eventdb` where user_id='$userid' limit 0,1";
 
 	$result = mysql_query($get_users) or die('Query 2 failed: ' . mysql_error());
 	return mysql_fetch_array($result, MYSQL_ASSOC);
@@ -38,11 +37,38 @@ function log_connection($eventid,$targetid,$scannerid, $comments){
 	}
 
 	$insert_query = "INSERT into `$eventdb` (`scanner_id`,`target_id`,`ip_addr`, `timestamp`, `user_agent`, `comments`) VALUES ('$scannerid', '$targetid', '$ip_addr', NOW(), '$user_agent','$comments')";
-	//echo $insert_query;
 	mysql_query($insert_query) or die('could not log: '.mysql_error());
 }
 
 function has_user_updated($userid, $eventid){
+	//boolean to test if user has updated their info yet
 	$userinfo=get_user_info($userid, $eventid);
 	return $userinfo['user_updated'];
+}
+
+function user_has_updated($userid, $eventid){
+	//update the db when user updates their info
+	$eventdb=get_db_name($eventid);
+	$userid=mysql_real_escape_string($userid);
+	$update_query = "UPDATE `$eventdb` SET `user_updated`='1' WHERE `user_id`='$userid'";
+
+	mysql_query($update_query) or die('could not update: '.mysql_error());
+}
+
+function update_info($userid, $eventid, $user_info){
+//takes an array of user data and updates database (generically based on array keys)
+	$eventdb=get_db_name($eventid);
+	$userid=mysql_real_escape_string($userid);
+
+	$update_query = "UPDATE `$eventdb` SET";
+
+	foreach($user_info as $key => $value){
+		$update_query .= " `".$key."` = '".mysql_real_escape_string($value)."',";
+	}
+	$update_query=rtrim($update_query, ',');
+	$update_query.= " WHERE `user_id`='$userid'";
+
+	echo "update query:" . $update_query;
+
+	mysql_query($update_query) or die('could not update: '.mysql_error());
 }
