eef1ad285400da07fb0cb9f72f1c1f405886918e
Charles Julian Knight <charles@s0journ-viii.rabidaudio.dnynds.org>
2013-03-02 12:20:30 -0500

    removed test code

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -6,18 +6,4 @@ $mysql_database = 'badger';
 $db=mysql_connect($mysql_host, $mysql_user, $mysql_password) or die('could not connect:'.mysql_error());
 mysql_select_db($mysql_database) or die('could not select database');
 
-
-//test
-echo "connected";
-$result = mysql_query("select * from 3daystartup") or die('Query failed: ' . mysql_error());
-
-echo "<table>\n";
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
 ?>
