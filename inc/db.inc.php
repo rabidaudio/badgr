7c8236139741d4b3f1676c183d30f3023a92a63c
Charles Julian Knight <charles@s0journ-viii.rabidaudio.dnynds.org>
2013-03-02 02:15:53 -0500

    Added db.inc.php to connect to database. includes test code to be removed.

---
+++ inc/db.inc.php
@@ -0,0 +1,23 @@
+<?php
+$mysql_host = 'localhost';
+$mysql_user = 'badger';
+$mysql_password = 'InTheKitchen11.';
+$mysql_database = 'badger';
+$db=mysql_connect($mysql_host, $mysql_user, $mysql_password) or die('could not connect:'.mysql_error());
+mysql_select_db($mysql_database) or die('could not select database');
+
+
+//test
+echo "connected";
+$result = mysql_query("select * from 3daystartup") or die('Query failed: ' . mysql_error());
+
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+?>
