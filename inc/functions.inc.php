0c43e10d1a7c05e2d35a00cee28e45d2e9c98f76
Charles Julian Knight <charles@s0journ-viii.rabidaudio.dnynds.org>
2013-03-02 16:03:56 -0500

    Added function to test if userinfo has been updated.
    also cleaned comments.

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -3,33 +3,31 @@
 include('db.inc.php');
 
 function get_db_name($eventid){
-	#echo "get_db_name called";
+	//helper function for db backend
+
 	$get_table = "select `event_db` from `events` where `events`.`event_id`=$eventid limit 0,1;";
 
 	$result = mysql_query($get_table) or die('Query 1 failed: ' . mysql_error());
 
 	$event_db = mysql_fetch_array($result, MYSQL_NUM);
-	#echo $event_db[0];
 	return $event_db[0];
 }
 
 function get_user_info($userid, $eventid){
-//Take a userid and eventid, and return an array
+//Take a userid and eventid, and return an array of user data
 	#echo "get_user_info called";
 
 	$eventdb=get_db_name($eventid);
-	#echo $eventdb;
 
 	$get_users = "select * from `$eventdb` where user_id=$userid limit 0,1";
 
 	$result = mysql_query($get_users) or die('Query 2 failed: ' . mysql_error());
-	#echo 'returning results';
 	return mysql_fetch_array($result, MYSQL_ASSOC);
 }
 
 
 function log_connection($eventid,$targetid,$scannerid, $comments){
-
+	//log every time someone scans another id
 	$eventdb=get_db_name($eventid);
 	$ip_addr=mysql_real_escape_string($_SERVER['REMOTE_ADDR']);
 	$user_agent=mysql_real_escape_string($_SERVER['HTTP_USER_AGENT']);
@@ -40,6 +38,11 @@ function log_connection($eventid,$targetid,$scannerid, $comments){
 	}
 
 	$insert_query = "INSERT into `$eventdb` (`scanner_id`,`target_id`,`ip_addr`, `timestamp`, `user_agent`, `comments`) VALUES ('$scannerid', '$targetid', '$ip_addr', NOW(), '$user_agent','$comments')";
-	echo $insert_query;
-	//mysql_query($insert_query) or die('could not log: '.mysql_error());
+	//echo $insert_query;
+	mysql_query($insert_query) or die('could not log: '.mysql_error());
+}
+
+function has_user_updated($userid, $eventid){
+	$userinfo=get_user_info($userid, $eventid);
+	return $userinfo['user_updated'];
 }
