8d4b262cd4f826a7f6944085de41ace196b9f541
rabidaudio <charles@rabidaudio.com>
2013-03-03 13:34:49 -0500

    Update db.inc.php

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_host = 'passionpathco.domaincommysql.com';
-$mysql_user = 'badger';
-$mysql_password = 'InTheKitchen11.';
-$mysql_database = 'badger';
+$mysql_host = '';
+$mysql_user = '';
+$mysql_password = '';
+$mysql_database = '';
 $db=mysql_connect($mysql_host, $mysql_user, $mysql_password) or die('could not connect:'.mysql_error());
 mysql_select_db($mysql_database) or die('could not select database');
 
